f47109a15e88c9ac28fb32972e12e802dda4ec3d
Keith Rudd <keith.rudd@gmail.com>
2017-02-25 19:58:39 -0500

    Testing

--- defineDomain.lua
+++ defineDomain.lua
@@ -1,12 +1,15 @@
 local torch = require 'torch'
 
 domain  = {}
-domain.boundary = {}
-domain.interior = {}
-domain.phi = {}
-domain.x = {}
-domain.y = {}
-domain.z = {}
+function domain:new(xlim,ylim,zlim,nPoints)
+    domain.boundary = {}
+    domain.interior = {}
+    domain.phi = {}
+    domain.x = {}
+    domain.y = {}
+    domain.z = {}
+end
+
 function domain:evalphi(x,y,z)
     --function to evaluate phi at x,y,z
 end
@@ -14,5 +17,6 @@ end
 function domain:defineBoundary(phi)
     --function to define the boundary based on the value of phi
 end
+
 
 return domain
